3ce27b2e3b0e3b6869f5b9b00a78058ed54fdcd9
Mikejiawei <15270013718@163.com>
2020-04-25 23:17:08 +0800

    add three tabs

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,8 @@ class App extends StatelessWidget {
       theme: ThemeData(
         brightness: Brightness.light,
         primarySwatch: Colors.yellow,
+        highlightColor: Color.fromRGBO(255, 255, 255, 0.5),
+        splashColor: Colors.white70.withOpacity(0.0)
       ),
     );
   }
@@ -20,7 +22,9 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return  DefaultTabController(
+      length: 3,
+      child: Scaffold(
       backgroundColor: Colors.grey[100],
         appBar: AppBar(
         leading: IconButton(
@@ -37,9 +41,27 @@ class Home extends StatelessWidget {
             ),
           ],
         elevation: 20.0, 
+        bottom: TabBar(
+          unselectedLabelColor: Colors.black38,
+          indicatorColor: Colors.black54,
+          indicatorSize: TabBarIndicatorSize.label,
+          indicatorWeight: 2.0,
+          tabs: <Widget>[
+            Tab(icon: Icon(Icons.local_airport)),
+            Tab(icon: Icon(Icons.local_florist)),
+            Tab(icon: Icon(Icons.change_history)),
+          ] 
         ),
-        body: null,
-      );
+        ),
+        body: TabBarView(
+          children: <Widget>[
+            Icon(Icons.local_airport, size: 128.0, color: Colors.black12),
+            Icon(Icons.local_florist, size: 128.0, color: Colors.black12),
+            Icon(Icons.change_history, size: 128.0, color: Colors.black12),
+          ],
+        ),
+      )
+    );
   }
 }
 
